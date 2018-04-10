804c16fa40f5f82a3373b1656431f96c6a492ff5
Isaac Avram <avrisaac555@gmail.com>
2018-04-09 18:38:41 -0700

    Added basic code for polyline

---
+++ HW1/polyline.js
@@ -0,0 +1,27 @@
+main(setup());
+
+function setup() {
+    var canvas = document.querySelector("#canvas");
+    var gl = canvas.getContext("webgl");
+
+    if(!gl) {
+        console.error("[Setup] Unable to initialize WebGL.");
+        alert("Unable to initialize WebGL.");
+        return;
+    }
+
+    gl.clearColor(0, 0, 0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    return {
+        canvas: canvas,
+        gl: gl
+    }
+}
+
+function main(context) {
+    if (!context) {
+        console.error("[Main] Recieved no context, exiting.")
+        return
+    }
+}
