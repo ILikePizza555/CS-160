eb506fab258379e9205b6bc55f1f9412b07e1c27
Isaac Avram <avrisaac555@gmail.com>
2018-04-13 18:11:50 -0700

    Point up and running

--- HW1/polyline.js
+++ HW1/polyline.js
@@ -1,5 +1,31 @@
+"use strict";
+
+const VSHADER_SOURCE = `
+    void main() {
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_PointSize = 10.0;
+    }
+`
+
+const FSHADER_SOURCE = `
+    void main() {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+`
+
 main(setup());
 
+/**
+ * The context object that setup() returns
+ * @typedef {Object} Setup~Context
+ * @property {Node} canvas - The object representing the canvas
+ * @property {WebGLRenderingContext} gl - The WebGl rendering context
+ */
+
+/**
+ * Sets up the WebGL program
+ * @returns {Setup~Context}
+ */
 function setup() {
     const canvas = document.querySelector("#canvas");
     const gl = canvas.getContext("webgl");
@@ -10,18 +36,29 @@ function setup() {
         return;
     }
 
+    if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.error("[Setup] Unable to initialize shaders");
+        return;
+    }
+
     gl.clearColor(0, 0, 0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     return {
         canvas: canvas,
         gl: gl
-    }
+    };
 }
 
+/**
+ * Main program
+ * @param {Setup~Context} context 
+ */
 function main(context) {
     if (!context) {
-        console.error("[Main] Recieved no context, exiting.")
-        return
+        console.error("[Main] Recieved no context, exiting.");
+        return;
     }
+
+    context.gl.drawArrays(context.gl.POINTS, 0, 1);
 }
