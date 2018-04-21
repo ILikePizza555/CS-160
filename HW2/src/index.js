464d0019dd03fa20594615b261acb043b64a3e09
Isaac Avram <avrisaac555@gmail.com>
2018-04-21 10:25:36 -0700

    We can now draw quads

--- HW2/src/index.js
+++ HW2/src/index.js
@@ -13,20 +13,32 @@ const glProgramConfig = {
     "vertexBuffer": {
         "size": 3,
         "name": "a_Position"
-    }
-}
+    },
+    "indexBuffer": true
+};
+
 /**
  * @param {GLProgram} prog 
  */
 function onLoad(prog) {
     "use strict";
     prog.setProgram();
-    prog.writePointsToVertexBuffer([new Point(-0.1, 0), new Point(0.1, 0), new Point(0, 0.1)]);
+    prog.writePointsToVertexBuffer([
+        new Point(-0.1, -0.1), //Bottom-left
+        new Point(-0.1, 0.1),  //Top-left
+        new Point(0.1, 0.1),   //Top-right
+        new Point(0.1, -0.1)   //Bottom-right
+    ]);
+
+    prog.writeToIndexBuffer(new Uint16Array([
+        0, 1, 2,
+        0, 3, 2
+    ]));
 
     context.clearColor(0.0, 0.0, 0.0, 1.0);
     context.clear(context.COLOR_BUFFER_BIT);
 
-    context.drawArrays(context.POINTS, 0, 3);
+    context.drawElements(context.LINE_STRIP, 6, context.UNSIGNED_SHORT, 0);
 }
 
 GLProgram.fromUrls(context, glProgramConfig).then(onLoad).catch(r => alert(r));
